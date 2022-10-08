34727737789fc5f6dcafe0d4972dcd4447ba5b9a
yangli <li002252@umn.edu>
2022-10-07 23:10:29 -0500

    add nox

---
+++ noxfile.py
@@ -0,0 +1,31 @@
+"""Nox sessions."""
+import sys
+from textwrap import dedent
+
+import nox
+
+try:
+    from nox_poetry import Session, session
+except ImportError:
+    message = f"""\
+    Nox failed to import the 'nox-poetry' package.
+    Please install it using the following command:
+    {sys.executable} -m pip install nox-poetry"""
+    raise SystemExit(dedent(message)) from None
+
+package = "mssw"
+python_versions = ["3.10", "3.9", "3.8", "3.7", "3.6"]
+nox.needs_version = ">= 2021.6.6"
+nox.options.sessions = ("tests",)
+
+
+@session(python=python_versions)
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    session.install(".")
+    session.install("coverage[toml]", "pytest", "pygments")
+    try:
+        session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
+    finally:
+        if session.interactive:
+            session.notify("coverage", posargs=[])
